abed369a80fa5e581e3accf6dbefe534b11330e6
chihebchannoufi <channoufichiheb@gmail.com>
2026-09-04 14:22:44 +0100

    Redirect home route to login

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::redirect('/', '/login');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
